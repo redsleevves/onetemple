46c9b442df0a69e9c2da7f72f8ae43cfcb7715ee
redsleevves <80672056+redsleevves@users.noreply.github.com>
2021-06-14 20:21:55 +0800

    Add files via upload

--- cart_api.php
+++ cart_api.php
@@ -1,26 +1,29 @@
-<?php include __DIR__. '/parts/config.php';
+<?php include __DIR__ . '/parts/config.php';
 
-if(! isset($_SESSION['cart'])){
+if (!isset($_SESSION['cart'])) {
     $_SESSION['cart'] = [];
 }
 
-$type = isset($_GET['type']) ? intval($_GET['type']) : 0;  // 哪一台車
+$type = isset($_GET['type']) ? $_GET['type'] : 0;  // 哪一台車
 $id = isset($_GET['id']) ? intval($_GET['id']) : 0;  // 商品
 
-switch($type){
+switch ($type) {
     case 'pdc':
-        if(! empty($id)){
+        if (!empty($id)) {
             unset($_SESSION['cart']['product'][$id]);
+            $_SESSION['cart']['product'] = array_values($_SESSION['cart']['product']);
         }
         break;
     case 'plan':
-        if(! empty($id)){
+        if (!empty($id)) {
             unset($_SESSION['cart']['plan'][$id]);
+            $_SESSION['cart']['plan'] = array_values($_SESSION['cart']['plan']);
         }
         break;
     case 'light':
-        if(! empty($id)){
+        if (!empty($id)) {
             unset($_SESSION['cart']['light'][$id]);
+            $_SESSION['cart']['light'] = array_values($_SESSION['cart']['light']);
         }
         break;
 }
